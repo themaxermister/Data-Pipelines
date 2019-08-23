a473a6a0933d39296dc5e2cf5f05731326706f45
Jeremy Chua <subscribingalerts@gmail.com>
2019-08-24 00:06:31 +0800

    Start

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -7,13 +7,16 @@ import helpers
 
 # Defining the plugin class
 class UdacityPlugin(AirflowPlugin):
+    
     name = "udacity_plugin"
+
     operators = [
         operators.StageToRedshiftOperator,
         operators.LoadFactOperator,
         operators.LoadDimensionOperator,
         operators.DataQualityOperator
     ]
+
     helpers = [
         helpers.SqlQueries
     ]
